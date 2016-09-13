c1edfcc117f7ccf8b1772b19344483cf8259ab28
vifespoir <vifespoir@gmail.com>
2016-09-13 16:44:47 +0200

    working on simplifying and ui

--- mLearningApp.py
+++ mLearningApp.py
@@ -37,11 +37,32 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 # Constants
 ALLOWED_EXTENSIONS = ['csv']
+SIMPLE_METHOD_NAMES = ['boxplot_all_quartiles', 'parallel_coordinates_graph', 'heatmap_pearson_correlation']
+COMPLEX_METHOD_NAMES = ['cross_plotting_pair_of_attributes', 'transpose_index', 'plot_target_correlation']
 
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def check_data_status():  # check if data should be normalized
+    try:
+        session['dataStatus'] = ast.literal_eval(request.args['normalized'])
+        logging.debug('Normalization Input: %s' % session['dataStatus'])
+        flash('Data Normalized: %s' % session['dataStatus'], 'alert-info')
+
+    except Exception:
+        session['dataStatus'] = False
+
+
+def check_if_session_active():
+    try:
+        session['filename']
+        return True
+    except KeyError:
+        flash('Session Expired', 'alert-danger')
+        return False
 
 
 @app.route('/')
@@ -54,19 +75,19 @@ def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            flash('No file part', 'alert-danger')
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
-            flash('No selected file')
+            flash('No selected file', 'alert-danger')
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
             session['filename'] = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], session['filename']))
-            flash('%s has been successfully uploaded' % session['filename'])
+            flash('%s has been successfully uploaded' % session['filename'], 'alert-success')
             session['name'], session['extension'] = session['filename'].split('.', 1)
             return redirect(url_for('visualize_options', name=session['name']))
 
@@ -77,7 +98,7 @@ def upload_file():
 def choose_file():
     try:
         files = listdir(app.config['UPLOAD_FOLDER'])
-        flash('%s file(s) found.' % len(files))
+        flash('%s file(s) found.' % len(files), 'alert-info')
     except OSError as e:
         logging.debug('Error when loading files %s' % e)
         return redirect(url_for('upload_file'))
@@ -104,81 +125,69 @@ def visualize_options(name):
     session.setdefault('filename', name)
     session.setdefault('name', name.split('.', 1)[0])
 
-    simpleMethodNames = ['boxplot_all_quartiles', 'parallel_coordinates_graph', 'heatmap_pearson_correlation']
-    complexMethodNames = ['cross_plotting_pair_of_attributes', 'transpose_index', 'plot_target_correlation']
-
-    session['simpleOptions'] = simpleMethodNames
-    session['complexOptions'] = complexMethodNames
+    session['simpleOptions'] = SIMPLE_METHOD_NAMES
+    session['complexOptions'] = COMPLEX_METHOD_NAMES
 
     logging.debug('Options visualized...')
-    return render_template('home.html')
+    return render_template('choose_plot.html')
 
 
-@app.route('/<name>/<methodName>', methods=['GET', 'POST'])
+@app.route('/<name>/simple/<methodName>', methods=['GET', 'POST'])
 def plot(name, methodName):
     logging.debug('App Plot starting...')
 
-    try:
-        dataStatus = ast.literal_eval(request.args['normalized'])
-        logging.debug('Normalization Input: %s' % dataStatus)
-        flash('Data Normalized: %s' % dataStatus)
-    except Exception:
-        dataStatus = False
+    session['methodName'] = methodName
 
-    try:
+    check_data_status()  # check if data should be normalized
+
+    if not check_if_session_active():  # check if a dataPlot model is being served
+        return redirect(url_for('choose_file'))
+
+    if session['methodName'] in SIMPLE_METHOD_NAMES:
         dataPlot = DataPlot(tableName='us-veggies',
                             dataFile='uploads/' + session['filename'],
-                            normalized=dataStatus)
-    except KeyError:
-        flash('Session Expired')
-        return redirect(url_for('choose_file'))
+                            normalized=session['dataStatus'])
+
+        logging.debug('dataPlot Status: %s' % dataPlot.normalized)
+        methods = dict(inspect.getmembers(dataPlot, predicate=inspect.ismethod))
+        method = methods[session['methodName']]
+        plot = method()
+        # Configure resources to include BokehJS inline in the document.
+        # For more details see:
+        #   http://bokeh.pydata.org/en/latest/docs/reference/resources_embedding.html#bokeh-embed
+        js_resources = INLINE.render_js()
+        css_resources = INLINE.render_css()
+        # For more details see:
+        #   http://bokeh.pydata.org/en/latest/docs/user_guide/embedding.html#components
+        script, div = components(plot.document(), INLINE)
+        return render_template('simple_plot.html',
+                               plot_script=script,
+                               plot_div=div,
+                               js_resources=js_resources,
+                               css_resources=css_resources,
+                               name=plot.plotName)
+
+    else:
+        logging.debug('URL_FOR %s' % session['methodName'])
+        return redirect(url_for(session['methodName'], name=session['name'], methodName=session['methodName']))
+
+
+@app.route('/<name>/complex/<methodName>', methods=['GET', 'POST'])
+def cross_plotting_pair_of_attributes(name, methodName):
+    logging.debug('App Cross Plot starting...')
+    dataPlot = DataPlot(tableName='us-veggies',
+                        dataFile='uploads/' + session['filename'],
+                        normalized=session['dataStatus'])
 
     logging.debug('dataPlot Status: %s' % dataPlot.normalized)
-    methods = dict(inspect.getmembers(dataPlot, predicate=inspect.ismethod))
-    method = methods[methodName]
-    plot = method()
-    # Configure resources to include BokehJS inline in the document.
-    # For more details see:
-    #   http://bokeh.pydata.org/en/latest/docs/reference/resources_embedding.html#bokeh-embed
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
-    # For more details see:
-    #   http://bokeh.pydata.org/en/latest/docs/user_guide/embedding.html#components
-    script, div = components(plot.document(), INLINE)
-    return render_template('embed.html',
-                           plot_script=script,
-                           plot_div=div,
-                           js_resources=js_resources,
-                           css_resources=css_resources,
-                           name=plot.plotName)
-
-
-@app.route('/<name>/cross_plotting_pair_of_attributes', methods=['GET', 'POST'])
-def cross_plotting_pair_of_attributes(name):
-    logging.debug('App Cross Plot starting...')
-
-    try:
-        dataStatus = ast.literal_eval(request.args['normalized'])
-        logging.debug('Normalization Input: %s' % dataStatus)
-        flash('Data Normalized: %s' % dataStatus)
-    except Exception:
-        dataStatus = False
-
-    try:
-        dataPlot = DataPlot(tableName='us-veggies',
-                            dataFile='uploads/' + session['filename'],
-                            normalized=dataStatus)
-    except KeyError:
-        flash('Session Expired')
-        return redirect(url_for('choose_file'))
-
     columns = dataPlot.data.columns
+
     if request.method == 'POST':
         logging.debug('POST form: %s' % [item for item in request.form.items()])
         firstCol, secondCol = request.form['firstCol'], request.form['secondCol']
 
         methods = dict(inspect.getmembers(dataPlot, predicate=inspect.ismethod))
-        method = methods['cross_plotting_pair_of_attributes']
+        method = methods[session['methodName']]
         plot = method(firstCol, secondCol)
 
         js_resources = INLINE.render_js()
@@ -186,7 +195,7 @@ def cross_plotting_pair_of_attributes(name):
 
         script, div = components(plot.document(), INLINE)
 
-        return render_template('cross_plotting_pair_of_attributes' + '.html',
+        return render_template(session['methodName'] + '.html',
                                columns=columns,
                                plot_script=script,
                                plot_div=div,
@@ -194,7 +203,7 @@ def cross_plotting_pair_of_attributes(name):
                                css_resources=css_resources,
                                name=plot.plotName)
 
-    return render_template('cross_plotting_pair_of_attributes' + '.html',
+    return render_template(session['methodName'] + '.html',
                            columns=columns,
                            plot_script=None,
                            plot_div=None,
@@ -203,26 +212,17 @@ def cross_plotting_pair_of_attributes(name):
                            name=None)
 
 
-@app.route('/<name>/transpose_index', methods=['GET', 'POST'])
-def transpose_index(name):
+@app.route('/<name>/complex/<methodName>', methods=['GET', 'POST'])
+def transpose_index(name, methodName):
+    logging.debug('App Transpose Index starting...')
+    dataPlot = DataPlot(tableName='us-veggies',
+                        dataFile='uploads/' + session['filename'],
+                        normalized=session['dataStatus'])
 
-    try:
-        dataStatus = ast.literal_eval(request.args['normalized'])
-        logging.debug('Normalization Input: %s' % dataStatus)
-        flash('Data Normalized: %s' % dataStatus)
-    except Exception:
-        dataStatus = False
-
-    try:
-        dataPlot = DataPlot(tableName='us-veggies',
-                            dataFile='uploads/' + session['filename'],
-                            normalized=dataStatus)
-    except KeyError:
-        flash('Session Expired')
-        return redirect(url_for('choose_file'))
+    logging.debug('dataPlot Status: %s' % dataPlot.normalized)
 
     methods = dict(inspect.getmembers(dataPlot, predicate=inspect.ismethod))
-    method = methods['transpose_index']
+    method = methods[session['methodName']]
     transpose_index = method()
 
     js_resources = INLINE.render_js()
@@ -235,27 +235,18 @@ def transpose_index(name):
                           plot_div=div,
                           name=fig))
 
-    return render_template('transpose_index' + '.html', js_resources=js_resources,
+    return render_template(session['methodName'] + '.html', js_resources=js_resources,
                            css_resources=css_resources, plots=plots)
 
 
-@app.route('/<name>/plot_target_correlation', methods=['GET', 'POST'])
-def plot_target_correlation(name):
+@app.route('/<name>/complex/<methodName>', methods=['GET', 'POST'])
+def plot_target_correlation(name, methodName):
+    logging.debug('App Plot Target Correlation starting...')
+    dataPlot = DataPlot(tableName='us-veggies',
+                        dataFile='uploads/' + session['filename'],
+                        normalized=session['dataStatus'])
 
-    try:
-        dataStatus = ast.literal_eval(request.args['normalized'])
-        logging.debug('Normalization Input: %s' % dataStatus)
-        flash('Data Normalized: %s' % dataStatus)
-    except Exception:
-        dataStatus = False
-
-    try:
-        dataPlot = DataPlot(tableName='us-veggies',
-                            dataFile='uploads/' + session['filename'],
-                            normalized=dataStatus)
-    except KeyError:
-        flash('Session Expired')
-        return redirect(url_for('choose_file'))
+    logging.debug('dataPlot Status: %s' % dataPlot.normalized)
 
     columns = dataPlot.numericData.columns
 
@@ -264,7 +255,7 @@ def plot_target_correlation(name):
         firstCol = request.form['firstCol']
 
         methods = dict(inspect.getmembers(dataPlot, predicate=inspect.ismethod))
-        method = methods['plot_target_correlation']
+        method = methods[session['methodName']]
         plot = method(firstCol)
 
         js_resources = INLINE.render_js()
@@ -272,7 +263,7 @@ def plot_target_correlation(name):
 
         script, div = components(plot.document(), INLINE)
 
-        return render_template('plot_target_correlation' + '.html',
+        return render_template(session['methodName'] + '.html',
                                columns=columns,
                                plot_script=script,
                                plot_div=div,
@@ -280,7 +271,7 @@ def plot_target_correlation(name):
                                css_resources=css_resources,
                                name=plot.plotName)
 
-    return render_template('plot_target_correlation' + '.html',
+    return render_template(session['methodName'] + '.html',
                            columns=columns,
                            plot_script=None,
                            plot_div=None,
@@ -290,4 +281,4 @@ def plot_target_correlation(name):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
